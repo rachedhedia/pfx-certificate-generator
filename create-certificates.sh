0bdf456e593329abd373eac09204da4dd1abc9c3
gatoos software <gatoos.software@gmail.com>
2019-05-05 16:50:56 +0200

    fix erase command in sh script, use rm instead and remove windows @echo off directive

--- create-certificates.sh
+++ create-certificates.sh
@@ -1,4 +1,3 @@
-@echo off
 docker run -it -v $PWD:/export frapsoft/openssl req -newkey rsa:2048 -nodes -keyout /export/privateKey.pem -x509 -days 365 -out /export/public-key-certificate.cer
 docker run -it -v $PWD:/export frapsoft/openssl pkcs12 -export -in /export/public-key-certificate.cer -inkey /export/privateKey.pem -out /export/private-key-certificate.pfx 
-erase privateKey.pem
+rm privateKey.pem
